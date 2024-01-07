365926279ca90eefbee55cab17592fe5df38f94c
Caitlin Jordan <jordan.cait17@gmail.com>
2024-01-07 15:40:14 -0800

    completed lab, bug with API site they used, switched to brewery db

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,10 +5,23 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+
+    URL = "https://api.openbrewerydb.org/v1/breweries"
 
     response = requests.get(URL)
     return response.content
+  
+  def program_types(self):
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+      programs_list.append(program["name"])
+    return programs_list
 
-programs = GetPrograms().get_programs()
-print(programs)
+# programs = GetPrograms().get_programs()
+# print(programs)
+programs = GetPrograms()
+types = programs.program_types()
+
+for name in set(types):
+  print(name)
